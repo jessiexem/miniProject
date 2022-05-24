5c50ea282fc4fa1605a37f8d679df51ebf82367a
jessiexem <jessiexem@hotmail.com>
2022-05-24 18:19:04 +0800

    rename app

--- src/main/java/sg/nus/iss/demoPAF/VocablifyApplication.java
+++ src/main/java/sg/nus/iss/demoPAF/VocablifyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnEnglishApplication {
+public class VocablifyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnEnglishApplication.class, args);
+		SpringApplication.run(VocablifyApplication.class, args);
 	}
 
 }
--- src/test/java/sg/nus/iss/demoPAF/VocablifyApplicationTests.java
+++ src/test/java/sg/nus/iss/demoPAF/VocablifyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LearnEnglishApplicationTests {
+class VocablifyApplicationTests {
 
 	@Test
 	void contextLoads() {
